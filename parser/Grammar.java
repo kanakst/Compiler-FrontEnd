0d61ac4701f7e300531afba63eff918b80e18fd4
Kanat Sovetov <abaiuly@kaist.ac.kr>
2020-05-25 19:17:00 +0900

    Started developing the Parser, currently the idea behind is to use Token tag values to distinguish between type of operators/statements to select.
    Maybe will have to switch to a smarter solution.

--- parser/Grammar.java
+++ parser/Grammar.java
@@ -1,9 +1,11 @@
 package parser;
 
-import lexer.*;
+import lexer.*; //is this even legal?
 import java.io.*;
 import java.util.*;
 
 public class Grammar {
+
     
+   
 }
--- parser/Parser.java
+++ parser/Parser.java
@@ -1,6 +1,30 @@
-import lexer.*;
+package parser;
 
+import lexer.*; // is this even legal?
 import java.io.*;
 import java.util.*;
 
+public class Parser {
 
+    private Token token;
+    
+    public Parser(Token t) {
+	token = t;
+    }
+
+    public void program() {
+	String tokenName;
+
+	tokenName = token.toString();
+	if(token.tag >= 500) { // tag range for operations/ statements
+
+	}
+	else if(token.tag >= 400 && token.tag < 500) {
+
+	}
+	else if(token.tag >= 300 && token.tag < 400) {
+
+	}
+	
+    }
+}
