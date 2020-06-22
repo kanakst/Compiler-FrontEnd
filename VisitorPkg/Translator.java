11f03f69a947eed734b14f8fca24658ba6b14fc7
Kanat Sovetov <abaiuly@kaist.ac.kr>
2020-06-22 14:03:37 +0900

    fully working compiler front-end

--- VisitorPkg/Translator.java
+++ VisitorPkg/Translator.java
@@ -1,11 +1,16 @@
 package VisitorPkg;
 
+import java.util.*;
+
 import newAST.*;
 import lexer.*;
 import newparser.*;
 
 public class Translator implements Visitor {
     private int assign_status = 0;
+    private int if_label = 1;
+    private int while_label = 1;
+    
     public void visit(TimesExpr n) {
         n.e1.accept(this);
         n.e2.accept(this);
@@ -48,10 +53,7 @@ public class Translator implements Visitor {
     }
 
     public void visit(Numerical n) {
-
-        
         System.out.println("push " + n.value);
-
     }
 
     
@@ -60,45 +62,55 @@ public class Translator implements Visitor {
         ++assign_status;
         n.id.accept(this);
 
-        // System.out.println();
-
         n.expr.accept(this);
 
-        System.out.println("Assign or :=");
+        System.out.println(":=");
 
     }
 
     public void visit(If n) {
+        
+        if_label = Math.max(if_label, while_label);
+
+        int local_label = if_label;
+        if_label = if_label + 1;
 
         n.expr.accept(this);
-
-        System.out.println("gofalse out");
-
+        
+        System.out.println("gofalse L" + local_label);
+        
         n.stmt.accept(this);
 
-        System.out.println("label out");
+        System.out.println("label L" + local_label);
+        
                
     }
 
     public void visit(While n) {
-
-        System.out.println("label test");
+        
+        while_label = Math.max(if_label, while_label);
+        
+        int true_label = while_label;
+        while_label = while_label + 1;
+        int false_label = while_label;
+        while_label = while_label + 1;
+        System.out.println("label L" + true_label); // 777
         
         n.expr.accept(this);
 
-        System.out.println("gofalse out");
+        System.out.println("gofalse L" + false_label); // 55
 
         n.stmt.accept(this);
 
-        System.out.println("goto test");
+        System.out.println("goto L" + true_label); //777
 
-        System.out.println("label out");
+        System.out.println("label L" + false_label); // 55
 
     }
 
     public void visit(Block n) {
 
-        System.out.println("{");
+        // System.out.println("{");
                       
         for (int i = 0; i < n.blockStmtList.size(); i++ ) {
        
@@ -106,7 +118,7 @@ public class Translator implements Visitor {
             //System.out.println();
         }
               
-        System.out.println("}");
+        //System.out.println("}");
         
     }
     
